bdb4550a25bdec8f0bab8c2faf75ff0ce26b1f65
Maria <58345565+Maria3107@users.noreply.github.com>
2023-10-12 10:12:06 -0300

    Add files via upload

---
+++ academiaSofttek/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Integer n1 =7;
+        Integer n2 =7;
+        Integer suma = n1+n2;
+        
+        System.out.println("La suma de " + n1 + " y " + n2 + " es: " + suma);
+            
+        
+    }
+    
+}
